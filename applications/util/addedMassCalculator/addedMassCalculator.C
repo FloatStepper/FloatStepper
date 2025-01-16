b19b2c5ef2afc617bd79286c3389f0b9d78f783b
isoAdvector <johan.roenby@gmail.com>
2025-01-16 19:20:23 +0100

    Renamed test-pisoAddedMass to addedMassCalculator and placed in utils. Cleaned added mass box and ellipse test cases.

---
+++ applications/util/addedMassCalculator/addedMassCalculator.C
@@ -0,0 +1,177 @@
+/*---------------------------------------------------------------------------*\
+    Module Name:       FloatStepper
+    Description:       OpenFOAM extension module for fluid-rigid body coupling
+    License:           GNU General Public License (GPL) version 3
+    Copyright:         2025 Johan Roenby, STROMNING APS
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of FloatStepper.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FloatStepper.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    addedMassCalculator
+
+
+Description
+    Testing added mass calculator class.
+    Based on interFoam to get its createFields.H.
+
+Author
+    Johan Roenby
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "dynamicFvMesh.H"
+#include "CMULES.H"
+#include "EulerDdtScheme.H"
+#include "localEulerDdtScheme.H"
+#include "CrankNicolsonDdtScheme.H"
+#include "subCycle.H"
+#include "immiscibleIncompressibleTwoPhaseMixture.H"
+#include "incompressibleInterPhaseTransportModel.H"
+#include "turbulentTransportModel.H"
+#include "pimpleControl.H"
+#include "fvOptions.H"
+#include "CorrectPhi.H"
+#include "fvcSmooth.H"
+#include "addedMass.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Solver for two incompressible, isothermal immiscible fluids"
+        " using VOF phase-fraction based interface capturing.\n"
+        "With optional mesh motion and mesh topology changes including"
+        " adaptive re-meshing."
+    );
+
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createDynamicFvMesh.H"
+    #include "initContinuityErrs.H"
+    #include "createDyMControls.H"
+    #include "createFields.H"
+
+    IOdictionary dynamicMeshDict
+    (
+        IOobject
+        (
+            "dynamicMeshDict",
+            mesh.time().constant(),
+            mesh,
+            IOobject::MUST_READ_IF_MODIFIED,
+            IOobject::NO_WRITE,
+            false
+        )
+    );
+
+    autoPtr<addedMass> addedM = addedMass::New
+    (
+        dynamicMeshDict.get<word>("addedMassModel"),
+        mesh,
+        dynamicMeshDict
+    );
+
+    // Determining active degrees of freedom
+    
+    Vector<bool> linDirs(dynamicMeshDict.getOrDefault
+    (
+        "linDirs", Vector<bool>(1, 1, 1))
+    );
+    Vector<bool> rotDirs(dynamicMeshDict.getOrDefault
+    (
+        "rotDirs", Vector<bool>(1, 1, 1))
+    );
+
+    // Ensure no linear motion along and no rotational motion transverse to
+    // empty directions
+    const Vector<label> solDirs = (mesh.solutionD() + Vector<label>::one)/2;
+    forAll(solDirs, i)
+    {
+        if (!solDirs[i])
+        {
+            linDirs[i] = 0;
+            rotDirs[(i + 1) % 3] = 0;
+            rotDirs[(i + 2) % 3] = 0;
+        }
+    }
+
+    FixedList<bool,6> enabledDoFs(false);
+
+    // Adding linear degrees of freedom to DoFs_
+    forAll(linDirs, i)
+    {
+        if (linDirs[i])
+        {
+            enabledDoFs[i] = true;
+        }
+    }
+
+    // Adding rotational degrees of freedom to DoFs_
+    forAll(rotDirs, i)
+    {
+        if (rotDirs[i])
+        {
+            enabledDoFs[i+3] = true;
+        }
+    }
+   Info << "Active degrees of freedom: " << enabledDoFs << endl;
+
+    OFstream osI("ma.dat");
+
+    // Reading center of rotation and orientation matrix from dicitonary
+    vector CoR = dynamicMeshDict.get<vector>("CofR");
+    tensor Q = dynamicMeshDict.get<tensor>("Q");
+    wordRes patches = dynamicMeshDict.get<wordRes>("patches");
+
+    // Added mass matrix
+    typedef SquareMatrix<scalar> SMatrix;
+    SMatrix Madd({6, 6}, 0);
+
+    Madd = addedM->computeAddedMass
+    (
+        enabledDoFs,
+        CoR,
+        Q,
+        patches
+    );
+
+    osI << " " << Madd << endl;
+    Info << "Madd = " << Madd << endl;
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
